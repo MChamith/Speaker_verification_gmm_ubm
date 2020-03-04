a2a93843ac1957a192d86d8442d176423ffc254d
MChamith <mawelacm@gmail.com>
2020-03-04 12:47:33 +0530

    fx
    Signed-off-by: MChamith <mawelacm@gmail.com>

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import shutil
 
 for i in range(10):
     count = 0
-    for root, dirs, filenames in os.walk('/home/ubuntu/volume/AudioMNIST/ByNumber/'+str(i)):
+    for root, dirs, filenames in os.walk('/home/ubuntu/volume/AudioMNIST/ByNumber/' + str(i)):
         for file in filenames:
-            print(file)
+            print('file ' + str(file) + 'count ' + str(count))
             if file.endswith('.wav'):
                 # number = file.split('/')[-2]
+                if count == 2500:
+                    break
                 path = os.path.join(root, file)
                 with open('development_set_enroll.txt', 'a') as fw:
                     fw.write(path + '\n')
