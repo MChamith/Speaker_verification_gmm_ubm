7d60aa1f31d767c2768001628ba4a3a4aa004e88
MChamith <mawelacm@gmail.com>
2020-03-10 07:35:20 +0530

    test fix
    Signed-off-by: MChamith <mawelacm@gmail.com>

--- test_speaker.py
+++ test_speaker.py
@@ -26,7 +26,7 @@ gmm_files = [os.path.join(modelpath, fname) for fname in
 models = [pickle.load(open(fname, 'rb')) for fname in gmm_files]
 speakers = [fname.split("/")[-1].split(".gmm")[0] for fname
             in gmm_files]
-ubm = pickle.load(open('universal_model/ubm_512_200_iter.gmm', 'rb'))
+ubm = pickle.load(open('timit_universal/ubm_512_100iter.gmm', 'rb'))
 # Read the test directory and get the list of test audio files
 correct = 0
 count = 0
